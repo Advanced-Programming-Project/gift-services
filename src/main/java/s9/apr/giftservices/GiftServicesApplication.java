ac1c013d3dcc239f48890e5ece54a594d4d57071
Akars <williamli973@gmail.com>
2023-02-22 16:09:43 +0100

    first commit

---
+++ src/main/java/s9/apr/giftservices/GiftServicesApplication.java
@@ -0,0 +1,13 @@
+package s9.apr.giftservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s9/apr/giftservices/GiftServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package s9.apr.giftservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
