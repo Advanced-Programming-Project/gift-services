46d2c2b1e2b300ee32a336c1c897ee23c8328d2d
martin <martin.lenaerts@hotmail.fr>
2023-03-11 11:53:17 +0100

    update containers

---
+++ src/test/java/s9/apr/giftservices/GiftServicesApplicationTests.java
@@ -0,0 +1,8 @@
+package s9.apr.giftservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftServicesApplicationTests {
+}
