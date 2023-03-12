65f8236cdb06a9abf9e1f9f56ff6fe349aff1b2b
martin <martin.lenaerts@hotmail.fr>
2023-03-12 17:26:25 +0100

    remove test directory

--- src/test/java/s9/apr/giftservices/GiftServicesApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package s9.apr.giftservices;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GiftServicesApplicationTests {
-}
